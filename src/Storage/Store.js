266f8193f5992bec94e2e7fa78f8bd951dba2662
Mateusz Martin <mateusz.martinn@gmail.com>
2023-05-24 13:01:35 +0200

    - Created `userSlice` to keep current user data. For now it is only name and email - Created utility states,reducers and selectors for state of `LogoutModal` and `isLoading` which indicates if page is loading or not. - Changed name `modalsSlice` to `utilitySlice` to keep it more readable and universal for the sake of those states purpose.

--- src/Storage/Store.js
+++ src/Storage/Store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './Slices/loginSlice';
 import registerReducer from './Slices/registerSlice';
-import modalsReducer from './Slices/modalsSlice';
-
+import utilityReducer from './Slices/utilitySlice';
+import userReducer from './Slices/userSlice';
 const Store = configureStore({
-  reducer: { login: loginReducer, register: registerReducer, modals: modalsReducer },
+  reducer: {
+    login: loginReducer,
+    register: registerReducer,
+    utility: utilityReducer,
+    currentUser: userReducer,
+  },
 });
 
 export default Store;
