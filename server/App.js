7755813f6a1932966b95d9f586b55109e26334a8
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2023-05-24 22:06:09 +0200

    server-DB-connect-feature

---
+++ server/App.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'node:path';
+import process from 'node:process';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const App = express();
+const formatsLogger = App.get('env') === 'development' ? 'dev' : 'short';
+App.use(morgan(`${formatsLogger}`));
+App.use(cors());
+App.use(express.json());
+App.use(express.static(path.join(process.cwd(), 'public')));
+
+App.use('/api/');
+
+App.use((req, res) => {
+  res.status(404).json({
+    status: 'error',
+    code: 404,
+    message: `Use api on routes`,
+    data: 'Not found',
+  });
+});
+
+// eslint-disable-next-line no-unused-vars
+App.use((err, req, res, next) => {
+  res.status(500).json({
+    status: 'fail',
+    code: 500,
+    message: err.message,
+    data: 'Internal Server Error',
+  });
+});
