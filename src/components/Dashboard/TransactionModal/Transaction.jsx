e9c8ad7017ba3d6e6d6bcf3021560907335a20d5
DawidKrupski <dawid.krupski323@gmail.com>
2023-05-25 19:26:07 +0200

    Add transaction modal window + css
    modal needs some tweaks to adapt its operation to the backend

---
+++ src/components/Dashboard/TransactionModal/Transaction.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import styles from './Transaction.module.css';
+import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
+
+export const TransactionModal = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('income');
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedExpense, setSelectedExpense] = useState([]);
+  const [category, setCategory] = useState('');
+
+  const addTransaction = () => {
+    setSelectedDate(new Date());
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSwitchToggle = () => {
+    setSelectedOption(prevOption => (prevOption === 'income' ? 'expense' : 'income'));
+    setSelectedExpense([]);
+  };
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
+
+  const validationSchema = Yup.object().shape({
+    transactionValue: Yup.number()
+      .typeError('Value must be a number')
+      .required('')
+      .test('valid-number', 'Invalid number', value => {
+        if (value === undefined || value === null) return false;
+        const decimalRegex = /^\d+(\.\d{0,2})?$/;
+        return decimalRegex.test(String(value)) && !String(value).includes('e');
+      }),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      transactionValue: '',
+    },
+    validationSchema,
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div>
+      <button onClick={addTransaction}>Click me</button>
+
+      {showModal && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <p className={styles.modalTitle}>Add transaction</p>
+            <div className={styles.optionContainer}>
+              <span
+                className={selectedOption === 'expense' ? styles.greyedText : styles.incomeColor}
+              >
+                Income
+              </span>
+              <label className={styles.switch}>
+                <input
+                  type="checkbox"
+                  checked={selectedOption === 'expense'}
+                  onChange={handleSwitchToggle}
+                />
+                <span className={`${styles.slider} ${styles.round}`}></span>
+              </label>
+              <span
+                className={selectedOption === 'income' ? styles.greyedText : styles.expenseColor}
+              >
+                Expense
+              </span>
+            </div>
+
+            {selectedOption === 'expense' && (
+              <div>
+                <select className={styles.categorySelect} defaultValue="">
+                  {category ? null : (
+                    <option value="" hidden>
+                      Select your option
+                    </option>
+                  )}
+                  <option value="Tutaj">Tutaj</option>
+                  <option value="Będą">Będą</option>
+                  <option value="Dodane">Dodane</option>
+                  <option value="Opcje">Opcje</option>
+                  <option value="ZBackendu">z Backendu</option>
+                </select>
+              </div>
+            )}
+            <div className={styles.valueAndTime}>
+              <input
+                type="text"
+                name="transactionValue"
+                placeholder="0.00"
+                className={styles.transactionValue}
+                {...formik.getFieldProps('transactionValue')}
+                onChange={e => {
+                  let value = e.target.value.replace(',', '.');
+                  value = value.replace(/[^0-9.]/g, '');
+                  const decimalPlaces = 2;
+                  const decimalIndex = value.indexOf('.');
+                  if (decimalIndex !== -1) {
+                    value = `${value.slice(0, decimalIndex + decimalPlaces + 1)}`;
+                  }
+                  formik.setFieldValue('transactionValue', value);
+                }}
+              />
+              {formik.touched.transactionValue && formik.errors.transactionValue ? (
+                <div className={styles.error}>{formik.errors.transactionValue}</div>
+              ) : null}
+              <div className={styles.transactionDate}>
+                <Datetime
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  dateFormat="DD-MM-YYYY"
+                  timeFormat={false}
+                  className={styles.datetimePicker}
+                />
+              </div>
+            </div>
+            <input className={styles.transactionComment} type="text" placeholder="Comment" />
+            <button className={styles.closeBtn} onClick={closeModal}>
+              &#10006;
+            </button>
+            <button type="submit" className={styles.addBtn} onClick={formik.handleSubmit}>
+              ADD
+            </button>
+            <button className={styles.cancelBtn} onClick={closeModal}>
+              CANCEL
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
