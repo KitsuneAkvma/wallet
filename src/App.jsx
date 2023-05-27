0761721dd02dd1daefefe3be3192fc4f7d898a6c
Mateusz Martin <mateusz.martinn@gmail.com>
2023-05-27 21:48:28 +0200

    Rebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { useState } from 'react';
-import reactLogo from '/svg/react.svg';
-import viteLogo from '/svg/vite.svg';
 import './App.css';
 import 'sanitize.css';
 import 'modern-normalize/modern-normalize.css';
 
+import Loader from './components/_General/Loader/Loader';
+
 function App() {
-  const [count, setCount] = useState(0);
-
-  const test = async () => {
-    setCount(count => count + 1);
-  };
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={test}>count is {count} print state</button>{' '}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  return <Loader />;
 }
 
 export default App;
