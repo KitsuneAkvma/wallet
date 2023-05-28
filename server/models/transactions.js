7cec25b5d045772e127d0a7ea331678af8cb30b1
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2023-05-28 18:19:59 +0200

    server/auth/routes/v3

---
+++ server/models/transactions.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+const dateNow = new Date().toString();
+export const transaction = new Schema(
+  {
+    transactionDate: {
+      type: String,
+      default: dateNow,
+    },
+    typeOfTransaction: {
+      type: String,
+      enum: ['Income', 'Expense'],
+      default: 'Expense',
+    },
+    categoryId: {
+      type: String,
+    },
+    comment: {
+      type: String,
+      default: '',
+    },
+    amountOfTransaction: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+export const Transaction = mongoose.model('transaction', transaction);
---
+++ server/routes/users.js
@@ -0,0 +1,26 @@
+import express from 'express';
+export const usersRouter = express.Router();
+import {
+  registration,
+  login,
+  logout,
+  verifyEmail,
+  secondVerifyEmail,
+  signout,
+  auth,
+} from '../services/controllers/authOperations.js';
+import { currentUser } from '../services/controllers/usersOperations.js';
+
+usersRouter.get('/current', auth, currentUser);
+
+usersRouter.post('/auth/sign-up', registration);
+
+usersRouter.get('/verify/:verificationToken', verifyEmail);
+
+usersRouter.post('/verify/', secondVerifyEmail);
+
+usersRouter.post('/auth/log-in', login);
+
+usersRouter.post('/auth/log-out', auth, logout);
+
+usersRouter.delete('/auth/sign-out', auth, signout);
