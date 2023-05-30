cdd41d1b503f7024ebf93adda01017db3a295c63
Mateusz Martin <mateusz.martinn@gmail.com>
2023-05-30 20:39:30 +0200

    Finished Header

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ import 'modern-normalize/modern-normalize.css';
 import { LazyRouter } from './utility/lazyComponents/lazyComponents';
 import Loader from './components/_General/Loader/Loader';
 import { ToastContainer } from 'react-toastify';
+import { useAuth } from './utility/hooks/useAuth';
+import Header from './components/_General/Header/Header';
 
 function App() {
+  const isLoggedIn = useAuth();
   return (
     <>
       <React.Suspense fallback={<Loader />}>
+        {isLoggedIn && <Header />}
         <LazyRouter />
       </React.Suspense>{' '}
       <ToastContainer />
