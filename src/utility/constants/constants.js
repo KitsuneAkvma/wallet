7500bec6c9d8a89e12c3acdd88fb5c283ba8eb5a
Krzysztof Flisikowski <kflisikowski@10g.pl>
2023-05-30 20:10:57 +0200

    currency page ready

--- src/utility/constants/constants.js
+++ src/utility/constants/constants.js
@@ -4,3 +4,10 @@ export const mediaQueries = {
   tablet: '(min-width: 768px) and (max-width: 1279px)',
   desktop: '(min-width: 1280px)',
 };
+
+export const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
