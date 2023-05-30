23183535ba1658304859895ae0ef6c0fb8deac21
Mateusz Martin <mateusz.martinn@gmail.com>
2023-05-30 21:19:02 +0200

    Redux selectors
    Created selectors for every state

---
+++ src/redux/selectors.js
@@ -0,0 +1,27 @@
+const selectSessionUser = state => state.session.user;
+const selectSessionLoginFormData = state => state.session.loginForm;
+const selectSessionRegisterFormData = state => state.session.registerForm;
+const selectSessionToken = state => state.session.token;
+const selectSessionIsAuth = state => state.session.isAuth;
+const selectSessionError = state => state.session.error;
+
+const selectGlobalIsLoading = state => state.global.isLoading;
+const selectGlobalModalLogoutState = state => state.global.isLoading;
+const selectGlobalModalTransactionState = state => state.global.isLoading;
+
+const selectFinanceBalance = state => state.finance.totalBalance;
+const selectFinanceIsLoading = state => state.finance.totalBalance;
+const selectFinanceData = state => state.finance.totalBalance;
+
+export {
+  selectSessionUser,
+  selectSessionLoginFormData,
+  selectSessionRegisterFormData,
+  selectSessionToken,
+  selectSessionIsAuth,
+  selectSessionError,
+};
+
+export { selectGlobalIsLoading, selectGlobalModalLogoutState, selectGlobalModalTransactionState };
+
+export { selectFinanceBalance, selectFinanceIsLoading, selectFinanceData };
